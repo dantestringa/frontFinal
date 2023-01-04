63e27d10ce7be21e2f8c94a7f99836fcf86e7aae
dntcuervo <dastringa03@gmail.com>
2023-01-04 20:50:51 -0300

    about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,63 +1,60 @@
 import React from 'react'
-import { useState, useEffect} from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 import Table from 'react-bootstrap/Table';
 
 
 const About = () => {
+        
+    const [datos, setDatos] = useState([]);
 
-  const [datos, setDatos] = useState([])
+    //api de mi data
+    //const URL = 'http://localhost:3001/datos';//variable local
 
-  //Api de mi database
-  //const URL = 'http://localhost:3001/datos'
+    const URL = 'https://backfinal-production-30ee.up.railway.app/datos'; //variable producción
 
-  const URL = 'https://backfinal-production-30ee.up.railway.app/datos'
-
-  //Creamos una funcion para useEffect
-  const getPersonas = async () => {
-    try {
-      const { data } = await axios.get(URL)
-      setDatos(data.datos)
-      console.log(data.datos);
-    } catch (error) {
-        console.log(error);
+    //Creamos una función para el useEffect
+    const getPersonas = async () => {
+        try {
+            const { data } = await axios.get(URL);
+            setDatos(data.datos)
+            console.log(data.datos);
+        } catch (error) {
+            console.error(error);
+        }
     }
-  }
 
-  useEffect(() => {
-    getPersonas()
-  }, [])
-
-
-  return (
-    <div className='section'>
-      <h2>
-        About Page
-      </h2>
-
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>DNI</th>
-          </tr>
-        </thead>
-        <tbody>
-        {datos.map(dato => 
-          <tr>
-            <td>{dato._id}</td>
-            <td>{dato.nombre}</td>
-            <td>{dato.apellido}</td>
-            <td>{dato.dni}</td>
-          </tr>
-        )}
-        </tbody>
-      </Table>
-
-    </div>
-  )
+    useEffect(() => {
+        getPersonas()
+    },[]);
+    
+        return (
+        <div className='section m-5'>
+            <h2>
+            Welcome to the about page
+            </h2>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nombre</th>
+                        <th>Apellido</th>
+                        <th>DNI</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {datos.map(dato => 
+                    <tr>
+                        <td>{dato._id}</td>
+                        <td>{dato.nombre}</td>
+                        <td>{dato.apellido}</td>
+                        <td>{dato.dni}</td>
+                    </tr>
+                )}
+                </tbody>
+            </Table>
+        </div>
+    )
 }
 
 export default About
